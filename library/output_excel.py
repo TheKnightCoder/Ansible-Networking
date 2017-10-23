d1a8468f3dcdac1b52b0581241cb5e0ccd805281
Russell <r>
2017-10-23 10:27:44 +0100

    added prompts to show cmd and config merge get facts output as excel (only working for facts)

---
+++ library/output_excel.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+import json
+import fasteners
+from pprint import pprint
+from ansible.module_utils.basic import *
+import os.path
+from openpyxl import Workbook
+from openpyxl import load_workbook
+
+#def WriteDictToCSV(csv_file,dict_data):
+#import csv
+#  try:
+#    file_exists = os.path.isfile(csv_file)
+#    with open(csv_file, 'ab') as csvfile:
+#      writer = csv.DictWriter(csvfile, dict_data.keys())
+#      if not file_exists:
+#        writer.writeheader()
+#      #for data in dict_data:
+#      writer.writerow(dict_data)
+#  except IOError as (errno, strerror):
+#    print("I/O error({0}): {1}".format(errno, strerror))
+#    module.fail_json(msg="I/O error({0}): {1}".format(errno, strerror))
+#  return   
+
+def WriteDictToXl(csv_file,dict_data):
+
+  file_exists = os.path.isfile(csv_file)
+  if file_exists:
+    wb = load_workbook(csv_file)
+    ws = wb.active
+  else:
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Facts"
+    for i in range(1,len(dict_data)):
+      ws.cell(row=1, column=i, value=dict_data.keys()[i])
+  
+  cur_row = ws.max_row+1
+  for i in range(1,len(dict_data)):
+    if(isinstance(dict_data.values()[i], list)):
+      dict_data.values()[i]= str('\n'.join(dict_data.values()[i]))
+    ws.cell(row=cur_row, column=i, value=str(dict_data.values()[i]))
+    
+  wb.save(csv_file)
+  return
+
+def main():
+  fields = {
+    "content": {"required": True, "type": "dict" },
+    "dest": {"required": True, "type": "str" },
+  }
+  
+  module = AnsibleModule(argument_spec=fields)
+  
+  facts = module.params['content']['ansible_facts']['napalm_facts']
+  try:
+    with fasteners.InterProcessLock('/tmp/ansible_lock_file'):
+      WriteDictToXl(module.params['dest'], facts)
+  except IOError as (errno, strerror):
+    print("I/O error({0}): {1}".format(errno, strerror))
+    module.fail_json(msg="I/O error({0}): {1}".format(errno, strerror))
+
+  module.exit_json(changed=True, meta=module.params)
+
+if __name__ == '__main__':  
+  main()
