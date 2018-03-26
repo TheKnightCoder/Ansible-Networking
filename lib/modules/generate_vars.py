17cb318ec01d4ac35e72f5906f9c24f393570819
Russell <r>
2018-03-26 13:38:22 +0100

    create generate vars example generate_vars module fix... create folder if not exist

--- lib/modules/generate_vars.py
+++ lib/modules/generate_vars.py
@@ -2,6 +2,7 @@
 import openpyxl
 from ansible.module_utils.basic import *
 import yaml
+import os
 def read_xls_dict(input_file, dest):
     "Read the XLS file and return as Ansible facts"
     result = {"ansible_facts":{}}
@@ -9,6 +10,7 @@ def read_xls_dict(input_file, dest):
     try:
         wb = openpyxl.load_workbook(input_file)
         spreadsheet = {}
+        
         if dest in wb.sheetnames:
           current_sheet = wb.get_sheet_by_name(dest)
           dict_keys = []
@@ -19,8 +21,15 @@ def read_xls_dict(input_file, dest):
               for c in range(2,current_sheet.max_column + 1):
                   temp_dict[dict_keys[c-2]] = current_sheet.cell(row=r,column=c).value
               spreadsheet[current_sheet.cell(row=r,column=1).value]=temp_dict
-         
-         
+          
+          #create host_vars/group_vars folder if it does not exist
+          try: 
+              os.makedirs(dest)
+          except OSError:
+              if not os.path.isdir(dest):
+                  raise
+          
+          
           for k in spreadsheet.keys():
               with open(dest+'/'+str(k),'w') as yaml_file:
                   out = yaml.safe_dump(spreadsheet[k], default_flow_style=False, explicit_start=True)
